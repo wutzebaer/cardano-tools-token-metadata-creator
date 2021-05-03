9226a08ae7a00274b970d0759079c409ddfcd104
peter <petergrossmann@email.de>
2021-05-03 12:04:14 +0200

    rm

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 docker build -t wutzebaer/cardano-tools-token-metadata-creator:latest .
 docker push wutzebaer/cardano-tools-token-metadata-creator:latest
 
-docker run -v %cd%:/work -w /work wutzebaer/cardano-tools-token-metadata-creator token-metadata-creator entry --init baa836fef09cb35e180fce4b55ded152907af1e2c840ed5218776f2f6d7961737365746e616d65
+docker run --rm -v %cd%:/work -w /work wutzebaer/cardano-tools-token-metadata-creator token-metadata-creator entry --init baa836fef09cb35e180fce4b55ded152907af1e2c840ed5218776f2f6d7961737365746e616d65
