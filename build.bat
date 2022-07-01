4b015af8499a7572fe60a237158202613b9fe2f0
peter <petergrossmann@email.de>
2022-07-01 22:41:23 +0200

    update

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-docker build --progress plain -t wutzebaer/cardano-tools-token-metadata-creator:latest .
-docker push wutzebaer/cardano-tools-token-metadata-creator:latest
+sudo docker build --progress plain -t wutzebaer/cardano-tools-token-metadata-creator:latest .
+sudo docker push wutzebaer/cardano-tools-token-metadata-creator:latest
